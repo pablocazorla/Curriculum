f3a29fd339ab82e6f29f824d81090f86535eaf32
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2016-02-20 19:59:19 -0300

    v20-02-16

---
+++ pcazorlaui_eng.php
@@ -0,0 +1,165 @@
+<!doctype HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Resume_PabloCazorla</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	
+	<!-- CSS -->
+	<link type="text/css" rel="stylesheet" href="css/style.css">
+	<link type="text/css" rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
+	<link type="text/css" rel="stylesheet" href="css/theme.css">
+	<link type="text/css" rel="stylesheet" href="css/pcazorlaui.css">
+	<style type="text/css">
+.skills li h5 {
+    width: 38%;
+}
+.skills li .bar {
+    width: 62%;
+}
+	</style>
+</head>
+<body>
+	<div class="page">
+		<div class="page-content">
+			<div class="summary">
+				<div class="portrait">
+					<img src="img/pablo-img.jpg">
+				</div>
+				<div class="summary-content">
+					<h1>Pablo Cazorla</h1>
+					<h2>UX Analyst / UI Developer</h2>
+					<ul class="clearfix basic-data-list">
+						<li>
+							<span class="icon"><i class="fa fa-calendar"></i></span> January 27, 1976
+						</li>
+						<li>
+							<span class="icon"><i class="fa fa-globe"></i></span> http://www.pcazorla.com
+						</li>
+						<li>
+							<span class="icon"><i class="fa fa-map-marker"></i></span> Mendoza, Argentina
+						</li>
+						<li>
+							<span class="icon"><i class="fa fa-envelope"></i></span> <span style="font-size:8pt">pablo.david.cazorla@gmail.com</span>
+						</li>
+						
+					</ul>
+				</div>
+			</div>
+			<div class="clearfix resume">
+				<div class="partial1">
+					<h3>About me</h3>
+					<p class="editable">
+						I'm UI and web designer, UX analyst, web developer and web illustrator. I love web illustration, interactive apps, user experience, web responsive, infographics, animation and everything about digital imaging.
+					</p>
+				</div>
+				<div class="partial2">
+					<h3>Work Experience</h3>
+					<ul class="item-list laboral">
+						<li>
+							<h4>Freelance designer (2016)</h4>
+							<p>Currently I am working on my own projects.</p>
+						</li>
+						<li>
+							<h4>Globant (2014-2015)</h4>
+							<p>Web UI designer desktop/mobile. Javascript/front-end developer.</p>
+						</li>
+						<li>
+							<h4>Huddle Group (2012-2014)</h4>
+							<p>UI developer Sr. web desktop and mobile. Javascript/front-end developer.</p>
+						</li>
+						<li>
+							<h4>MercadoLibre (2010-2012)</h4>
+							<p>UX analyst, webdesigner and javascript/front-end developer.</p>
+						</li>
+						<li>
+							<h4>Inamika Interactive (2008-2010)</h4>
+							<p>Senior designer multimedia, web designer y flash animator and digital illustrator.</p>
+						</li>
+						<li>
+							<h4>SVA Informatic Solutions (2006-2007)</h4>
+							<p>Web developer and multimedia designer.</p>
+						</li>
+					</ul>
+					<h3>Education</h3>
+					<ul class="item-list education">
+						<li>
+							<h4>Visual Arts an Design (1999-2003)</h4>
+							<p>Faculty of Arts with a orientation in Visual Arts (specializing in Digital Art and Design) Mendoza, Argentina.</p>
+						</li>
+					</ul>
+				</div>
+				<div class="partial3">
+					<h3>Technical Skills</h3>
+					<ul class="skills">
+						<li>
+							<h5>HTML5</h5>
+							<div class="bar"><div style="width:95%"></div></div>
+						</li>
+						<li>
+							<h5>CSS3</h5>
+							<div class="bar"><div style="width:98%"></div></div>
+						</li>
+						<li>
+							<h5>Javascript</h5>
+							<div class="bar"><div style="width:90%"></div></div>
+						</li>
+						<li>
+							<h5>JQuery</h5>
+							<div class="bar"><div style="width:92%"></div></div>
+						</li>
+						<li>
+							<h5>LESS, Bootstrap</h5>
+							<div class="bar"><div style="width:80%"></div></div>
+						</li>
+						<li>
+							<h5>Wordpress</h5>
+							<div class="bar"><div style="width:70%"></div></div>
+						</li>						
+						<li>
+							<h5>GIT and SVN</h5>
+							<div class="bar"><div style="width:50%"></div></div>
+						</li>
+						<li>
+							<h5>MVVM frameworks</h5>
+							<div class="bar"><div style="width:35%"></div></div>
+						</li>
+						<li>
+							<h5>Adobe Photoshop</h5>
+							<div class="bar"><div style="width:92%"></div></div>
+						</li>
+						<li>
+							<h5>Adobe Illustrator</h5>
+							<div class="bar"><div style="width:80%"></div></div>
+						</li>
+					</ul>
+					<h3>Expertise</h3>
+					<ul class="item-list knowledge">
+						<li>
+							<h4>UI development</h4>
+							<p>Front-end development, HTML5/CSS3 applications, Canvas API and SVG, JS libraries (MVVM), apps mobile, CMS (eg: Wordpress), SEO and web analytics.</p>
+						</li>
+						<li>
+							<h4>Design and Web UX</h4>
+							<p>Logos and icons, branding and advertising, presentations, infographics and process illustration, usability improve, A/B testing, information architecture, statistics and usage analysis.</p>
+						</li>
+					</ul>
+				</div>
+				
+			</div>
+		</div>
+		<footer>
+<i class="fa fa-envelope-o"></i> pablo.david.cazorla@gmail.com
+		</footer>
+	</div>
+	
+
+	<script src="js/libs/jquery-1.11.2.min.js"></script>
+	<script src="js/jquery.editable.js"></script>
+	<script src="js/app.js"></script>
+</body>
+</html>
